e3d50c600c4c3d88949fe2c7e4b0dfd861bcd1cc
kovacsandrasoe <kovacs.andras@nik.uni-obuda.hu>
2019-10-16 19:47:18 +0200

    final

--- ApiConsumer/MainWindow.xaml.cs
+++ ApiConsumer/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +21,51 @@ namespace ApiConsumer
     /// </summary>
     public partial class MainWindow : Window
     {
+        static HttpClient client;
         public MainWindow()
         {
             InitializeComponent();
+            client = new HttpClient();
+            client.BaseAddress = new Uri("https://oeapika.azurewebsites.net");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue
+                ("application/json"));
+
+            GetAll();
+        }
+
+        async Task AddToDo(Todo t)
+        {
+            HttpResponseMessage response =
+                await client.PostAsJsonAsync("/api/values", t);
+
+            response.EnsureSuccessStatusCode();
+
+            GetAll();
+        }
+
+        async Task<List<Todo>> GetAll()
+        {
+            List<Todo> todos = new List<Todo>();
+            HttpResponseMessage response = await
+                client.GetAsync("/api/values");
+            if (response.IsSuccessStatusCode)
+            {
+                //
+                todos = await response.Content.ReadAsAsync<List<Todo>>();
+            }
+            dgrid.ItemsSource = todos;
+            return todos;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Todo t = new Todo()
+            {
+                Name = tb_name.Text
+            };
+            AddToDo(t);
         }
     }
 }
